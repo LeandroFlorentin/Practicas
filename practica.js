921ab9e087caf496c1015849ae3fd7d17d1dbc35
Leandro Florentin <leandro.florentin@hotmail.com>
2022-03-30 17:35:30 -0300

    aceptar o rechazar alumnos

--- practica.js
+++ practica.js
@@ -1,42 +1,50 @@
-const obtenerInformacion = (materia)=> {
-    materias = {
-        fisica : ["Sabato","pedro","juan","hernesto","jose","leandro"],
-        programacion : ["Borges","pedro","juan","hernesto","jose"],
-        logica : ["Cervantes","pedro","juan","hernesto",,"leandro"],
-        quimica : ["Conrad","pedro","jose","leandro"]
-    }
-    if(materias[materia] !== undefined){
-        return [materias[materia],materia,materias];
+let materias = {
+    fisica : ["Sabato","pedro","juan","hernesto","jose","leandro"],
+    programacion : ["Borges","pedro","juan","hernesto","jose"],
+    logica : ["Cervantes","pedro","juan","hernesto",,"leandro"],
+    quimica : ["Conrad","pedro","jose","leandro"]
+}
+const inscribir = (alumno,materia)=>{
+    personas = materias[materia];
+    if(personas.length >= 21){
+        document.write(`${alumno} las clases de ${materia} ya estan llenas<br>`)
     }else{
-        return materias;
-    }
-}
-const mostrarInformacion = (materia)=>{
-    let informacion = obtenerInformacion(materia);
-if (informacion !== false){
-    let profesor = obtenerInformacion(materia)[0][0];
-    let alumnos = obtenerInformacion(materia)[0];
-    alumnos.shift()
-    document.write(`el profesor de ${informacion[1]} es : ${profesor} <br> y los alumnos son: ${alumnos}
-    <br><br>
-    `)
-}
-}
-const cantidadDeClases = (alumno)=>{
-    let informacion = obtenerInformacion();
-    let clasesPresentes=[];
-    let cantidadTotal = 0;
-    for (info in informacion){
-        if (informacion[info].includes(alumno)){
-         cantidadTotal++;
-         clasesPresentes.push(" " + info);
+        personas.push(alumno);
+        if(materia == "fisica"){
+        materias = {
+            fisica : personas,
+            programacion : materias["programacion"],
+            logica : materias["logica"],
+            quimica : materias["quimica"]
         }
+        }
+        else if(materia == "programacion"){
+            materias = {
+                fisica : materias["fisica"],
+                programacion : personas,
+                logica : materias["logica"],
+                quimica : materias["quimica"]
+            }
+        }
+        else if(materia == "logica"){
+            materias = {
+                fisica : materias["fisica"],
+                programacion : materias["programacion"],
+                logica : personas,
+                quimica : materias["quimica"]
+            }
+        }
+        else if(materia == "quimica"){
+            materias = {
+                fisica : materias["fisica"],
+                programacion : materias["programacion"],
+                logica : materias[logica],
+                quimica : personas
+            }
+        }
+        document.write(`${alumno} se inscribio con exito a ${materia}<br>`)
     }
-    return `${alumno} esta en ${cantidadTotal} clases: ${clasesPresentes}<br><br>`
 }
-mostrarInformacion("fisica")
-mostrarInformacion("programacion")
-mostrarInformacion("logica")
-mostrarInformacion("quimica")
-document.write(cantidadDeClases("leandro"))
-document.write(cantidadDeClases("jose"))
+document.write(materias["fisica"]+"<br>")
+inscribir("jere","fisica")
+document.write(materias["fisica"])
